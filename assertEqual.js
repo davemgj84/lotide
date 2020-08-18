8c0155d6309abbde03fb66e5519175f969f77f26
davemgj84 <davemgj@gmail.com>
2020-08-18 01:20:08 +0000

    Initial Commit of assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// Test Code:
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Hello", "Goodbye");
+assertEqual(3, 3);
+assertEqual(4, 2);
