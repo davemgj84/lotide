20f7d37f2e0836541ad1c4308a57dfcc46e8152c
davemgj84 <davemgj@gmail.com>
2020-08-19 23:27:28 +0000

    Implemented function to find letter positions in a string

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// +++++ TESTS +++++
+
+assertArraysEqual(letterPositions("Bootcamp is very challenging").g, [24, 27]);
+assertArraysEqual(letterPositions("David").v, [2]);
