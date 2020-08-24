00a4afa2cfc07d6dc28b55e5d081a00016452bde
davemgj84 <davemgj@gmail.com>
2020-08-24 20:20:06 +0000

    created test folder, started moving tests into separate files - assertEqual && head so far

--- assertEqual.js
+++ assertEqual.js
@@ -7,13 +7,3 @@ const assertEqual = function(actual, expected) {
 };
 
 module.exports = assertEqual;
-
-// +++++ TESTS ++++++
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
-// assertEqual("Lighthouse Labs", "Lighthouse Labs");
-// assertEqual("Hello", "Goodbye");
-// assertEqual(3, 3);
-// assertEqual(4, 2);
---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// assertEqual Tests
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Hello", "Goodbye");
+assertEqual(3, 3);
+assertEqual(4, 2);
